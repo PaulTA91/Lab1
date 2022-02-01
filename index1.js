b2fdb7d2bb737a4afff023963f399065307c7171
PaulTA91 <parmst202@caledonian.ac.uk>
2022-02-01 14:39:26 +0000

    vscode test

--- index1.js
+++ index1.js
@@ -2,6 +2,6 @@ const http = require('http');
 http.createServer(function(req, res) {
 res.writeHead(200,
 { 'Content-type': 'text/plain' });
-res.end('Hello from my Node.js application!');
+res.end('Hello from my Node.js application!!');
 }).listen(3000);
 console.log('Server started on port 3000, ctrl^c to quit.');
