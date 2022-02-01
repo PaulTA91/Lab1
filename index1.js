89f71a5c0e9c401fec1bdebe77210e8abd946563
PaulTA91 <parmst202@caledonian.ac.uk>
2022-02-01 15:22:35 +0000

    html working

--- index1.js
+++ index1.js
@@ -1,7 +1,9 @@
 const http = require('http');
 http.createServer(function(req, res) {
 res.writeHead(200,
-{ 'Content-type': 'text/plain' });
-res.end('Hello from my Node.js application!');
+{ 'Content-type': 'html' });
+res.write('<h1>welcome to my application!</h1>');
+res.write('<div>This response is sent using node.js</div>');
+res.end('<div>Have a nice day!</div>');
 }).listen(3000);
 console.log('Server started on port 3000, ctrl^c to quit.');
